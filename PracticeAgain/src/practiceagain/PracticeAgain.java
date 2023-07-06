e2cc5cab7efdde5e57997d615d60a61183732695
Tynesp <nicollti@sheridancollege.ca>
2023-07-06 11:18:58 -0400

    Practice makes perfect

---
+++ PracticeAgain/src/practiceagain/PracticeAgain.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practiceagain;
+
+/**
+ *
+ * @author tenes
+ */
+public class PracticeAgain {
+
+    /**
+     * @param args the command line arguments
+     *///WE Are Practicing again !!!!!!!!!!!!
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
